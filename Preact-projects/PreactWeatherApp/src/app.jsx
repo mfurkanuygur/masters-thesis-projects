7efe8e703d853d6274adb42270036c8dc4f5a581
M.Furkan Uygur <76523870+mfurkanuygur@users.noreply.github.com>
2023-12-31 18:20:54 +0300

    preact weather app ok

--- Preact-projects/PreactWeatherApp/src/app.jsx
+++ Preact-projects/PreactWeatherApp/src/app.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import Router from 'preact-router';
+
 import './app.css'
+import EnterLocation from './components/EnterLocation';
+import DetailPage from './components/DetailPage';
+import { useState } from 'preact/hooks';
 
 export function App() {
-  const [count, setCount] = useState(0)
+
+  const [locationData, setLocationData] = useState()
+  const [cityName, setCityName] = useState("Kayseri")
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <Router>
+      <EnterLocation path="/" locationData={locationData} setLocationData={setLocationData} cityName={cityName} setCityName={setCityName}/>
+      <DetailPage path="/:id" />
+
+    </Router>
   )
 }
---
+++ Preact-projects/PreactWeatherApp/src/components/EnterLocation.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from 'preact/hooks'
+// import PropTypes from 'prop-types'
+import { getApiData } from '../request/request';
+import DisplayLocationData from './DisplayLocationData';
+import Loading from './Loading';
+
+const EnterLocation = ({ locationData, setLocationData, cityName, setCityName }) => {
+    const inputRef = useRef()
+
+    useEffect(() => {
+        if (cityName == "Kayseri") {
+            getApiData("Kayseri").then(data => { setLocationData(data) })
+        }
+        else {
+            getApiData(cityName).then(data => { setLocationData(data) })
+        }
+    }, [])
+
+    const search = (e) => {
+        e.preventDefault();
+        getApiData(inputRef?.current?.value).then(data => { setLocationData(data), setCityName(inputRef?.current?.value) })
+    }
+
+    return (
+        <div>
+            <form onSubmit={(e) => search(e)}>
+                <input type="text" placeholder="Enter a location!" ref={inputRef} />
+                <button type="submit">Search</button>
+            </form>
+            {
+                locationData == null ? <Loading /> : <DisplayLocationData locationData={locationData} />
+            }
+        </div>
+    )
+    // https://api.openweathermap.org/data/2.5/weather?q=sivas&appid=057275c30dc7aecc34c49be69110d6d1&lang=tr&units=metric
+    // https://api.openweathermap.org/data/2.5/forecast?q=sivas&appid=057275c30dc7aecc34c49be69110d6d1&lang=tr&units=metric
+
+}
+
+// EnterLocation.propTypes = {
+//     cityName: PropTypes.string,
+//     setCityName: PropTypes.func,
+//     locationData: PropTypes.object,
+//     setLocationData: PropTypes.func
+// }
+export default EnterLocation
